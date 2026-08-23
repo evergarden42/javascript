4a1196fda7533fdcdfa6205ae5034f970dcb50f2
evergarden42 <162397905+evergarden42@users.noreply.github.com>
2026-08-24 04:08:47 +0800

    Add consts

---
+++ datacons/strings.js
@@ -0,0 +1,11 @@
+const string = "The revolution will not be televised.";
+console.log(string);
+
+const backtick = `meow`;
+console.log(backtick);
+
+const cat = `the cats\' sound goes ${backtick} ${backtick}`
+console.log(cat);
+
+Number()
+String()
